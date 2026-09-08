deda0073c2ef02fa53de98f471931b686a450d6e
Ole Peko S├©rensen <ole@peko.net>
2026-09-08 21:30:27 +0200

    Removed link from logo on front page

--- elements/toc.php
+++ elements/toc.php
@@ -10,11 +10,11 @@
   </div>
 
   <div class="top-heading-title">
-    <a class="top-heading-logo" href="/" aria-label="Pekosoft">
+    <div class="top-heading-logo" aria-label="Pekosoft">
       <svg class="assets" viewBox="0 0 512 101.87" role="img" aria-label="Pekosoft">
         <use href="/assets.svg#logo" />
       </svg>
-    </a>
+    </div>
     <h1 class="release-title">
       <?php echo $releaseName; ?><?php if ($releasePage !== ''): ?> <span><?php echo $releasePage; ?></span><?php endif; ?>
     </h1>
